047b1bde48ef43a4fe97df7edec58b90cff69480
GabrielCarvalho17 <gabrielcarvalho17@outlook.com>
2025-04-01 07:47:03 -0300

    Accordion com Animations do Angular

--- src/app/components/accordion/accordion.component.ts
+++ src/app/components/accordion/accordion.component.ts
@@ -1,18 +1,28 @@
 import { Component, Input } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-accordion',
+  standalone: true,
   imports: [NgFor],
   templateUrl: './accordion.component.html',
   styleUrl: './accordion.component.scss',
+  animations: [
+    trigger('collapse', [
+      state('open', style({ height: '*', overflow: 'hidden' })),
+      state('closed', style({ height: '0', overflow: 'hidden' })),
+      transition('open <=> closed', animate('300ms ease')),
+    ]),
+  ],
 })
 export class AccordionComponent {
   @Input() title = '';
   @Input() items: string[] = [];
+
   isOpen = false;
 
-  toggleAccordion() {
+  toggle() {
     this.isOpen = !this.isOpen;
   }
 }
